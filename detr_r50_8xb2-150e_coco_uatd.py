4eefb4177dafd051c31e07d5ebf7f304096a978e
Hannan Ejaz Keen <keenhannan@gmail.com>
2023-11-22 10:45:50 +0100

    Added new pipeline

--- detr_r50_8xb2-150e_coco_uatd.py
+++ detr_r50_8xb2-150e_coco_uatd.py
@@ -6,30 +6,65 @@ model = dict(
         bbox_head=dict(num_classes=1))
 
 # Modify dataset related settings
-data_root = '/media/hannan/samsung/Dataset/UATD_Dataset/UATD_COCO/images/'
+data_root = '/mnt/public_work/Datasets/External/Object_Detection/Underwater_Detection/UATD/images/'
 metainfo = {
     'classes': ('object', ),
     'palette': [
         (220, 20, 60),
     ]
 }
+
+max_epochs = 12
+backend_args = None
+
+#train_pipeline = [
+    #dict(type='LoadImageFromFile', backend_args=backend_args),
+    #dict(type='LoadAnnotations', with_bbox=True),
+    #dict(type='Resize', scale=(416, 416), keep_ratio=True),
+    #dict(type='RandomFlip', prob=0.5),
+    #dict(type='PhotoMetricDistortion', brightness_delta=32, contrast_range=(0.5, 1.5), saturation_range=(0.5, 1.5), hue_delta=18),
+    #dict(type='PackDetInputs')
+#]
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=(416, 416), keep_ratio=True),
+    # If you don't have a gt annotation, delete the pipeline
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
 train_dataloader = dict(
-    batch_size=1,
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
         ann_file='train/annotation_coco.json',
-        data_prefix=dict(img='train/')))
+        data_prefix=dict(img='train/'),
+        pipeline=_base_.train_pipeline))
 val_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
         ann_file='val/annotation_coco.json',
-        data_prefix=dict(img='val/')))
+        data_prefix=dict(img='val/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args))
 test_dataloader = val_dataloader
 
 # Modify metric related settings
-val_evaluator = dict(ann_file=data_root + 'val/annotation_coco.json')
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'val/annotation_coco.json',
+    metric='bbox',
+    format_only=False,
+    backend_args=backend_args)
 test_evaluator = val_evaluator
 
 # We can use the pre-trained Mask RCNN model to obtain higher performance
--- faster-rcnn_r50_fpn_1x_coco_uatd.py
+++ faster-rcnn_r50_fpn_1x_coco_uatd.py
@@ -7,30 +7,71 @@ model = dict(
         bbox_head=dict(num_classes=1)))
 
 # Modify dataset related settings
-data_root = '/media/hannan/samsung/Dataset/UATD_Dataset/UATD_COCO/images/'
+data_root = '/mnt/public_work/Datasets/External/Object_Detection/Underwater_Detection/UATD/images/'
 metainfo = {
     'classes': ('object', ),
     'palette': [
         (220, 20, 60),
     ]
 }
+
+backend_args = None
+
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=(416, 416), keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion', brightness_delta=32, contrast_range=(0.5, 1.5), saturation_range=(0.5, 1.5), hue_delta=18),
+    dict(type='PackDetInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=(416, 416), keep_ratio=True),
+    # If you don't have a gt annotation, delete the pipeline
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
 train_dataloader = dict(
-    batch_size=1,
+    batch_size=4,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
         ann_file='train/annotation_coco.json',
-        data_prefix=dict(img='train/')))
+        data_prefix=dict(img='train/'),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline,
+        backend_args=backend_args))
 val_dataloader = dict(
+    batch_size=4,
+    num_workers=4,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
-        ann_file='val/annotation_coco.json',
-        data_prefix=dict(img='val/')))
+        ann_file='test_tmp/annotation_coco.json',
+        data_prefix=dict(img='test_tmp/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args))
 test_dataloader = val_dataloader
 
 # Modify metric related settings
-val_evaluator = dict(ann_file=data_root + 'val/annotation_coco.json')
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'test_tmp/annotation_coco.json',
+    metric='bbox',
+    format_only=False,
+    backend_args=backend_args)
 test_evaluator = val_evaluator
 
 # We can use the pre-trained Mask RCNN model to obtain higher performance
